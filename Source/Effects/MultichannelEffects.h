e471896ef62ee41d1e5233403243eb17596186d3
LukeMAmos <lukeamos2111@gmail.com>
2026-07-30 16:45:42 +0100

    Implementation of Multichannel effect , which is a template with functions to turn a single mono effect into an effect which can be used on multiple channels

--- Source/Effects/MultichannelEffects.h
+++ Source/Effects/MultichannelEffects.h
@@ -1,8 +1,37 @@
-#include "EffectDelay.h"
-#include "EffectDistortion.h"
-#include "EffectFilter.h"
-#include "EffectReverb.h"
+//Implementation of multichannel versions of the Effects
 
-//Implementation of multichannel versions of the Effects 
-
+template<typename EffectType>
+class MultichannelEffect{
+    
+public:
+    
+    template<typename... Args>
+    void prepare(int numChannels , Args&&... args){
+        
+        effects.resize(nChannels);
+        
+        for(auto& effect : effects)
+            effect.prepare(std::forward<Args>(args)...);
+        
+    }
+    
+    template<typename... ParamArgs>
+    void setParameters(ParamArgs&&... paramArgs){
+        
+        for(auto& effect : effects)
+            effect.setParameters(std::forward<ParamArgs>(paramArgs)...);
+        
+    }
+    
+    float process(int channel , float input){
+        
+        return effects[channel].process(input);
+    }
+    
+    int getNumChannels(){
+        return effects.size();
+    }
+private:
+    std::vector<EffectType> effects;
+};
 
