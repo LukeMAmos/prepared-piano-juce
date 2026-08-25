a2a532d306348d2bb4da89608288123ec0bf8aa2
LukeMAmos <lukeamos2111@gmail.com>
2026-08-25 18:40:39 +0100

    Additional file for Grid patterns used by the custom menu element

--- Source/UI/MenuDesign.h
+++ Source/UI/MenuDesign.h
@@ -4,11 +4,21 @@
 #pragma once
 #include <juce_gui_basics/juce_gui_basics.h>
 
+using GridPattern = std::array<std::array<bool , 6> , 3>;
+
 class OscMenu : public juce::ComboBox{
     
+public:
     OscMenu();
+    //Override the paint and mouse down functions, mouse down functionality should loop through elements with each press 
     void paint(juce::Graphics& g) override;
-    
     void mouseDown(const MouseEvent&) override;
+    
+    //Functions for passing through the grid arrays
+    void setGridPatterns(std::vector<GridPattern> patterns);
+    
+private:
+    
+    std::vector<GridPattern> storedPatterns;
     
 };
